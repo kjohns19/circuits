9935f1302f20612b983b4a765acce1015713be22
Kevin Johnson <krjohnson111@gmail.com>
2018-08-15 18:40:54 -0400

    Edit tool no longer moves the component's center to the mouse

--- tools/edit_tool.py
+++ tools/edit_tool.py
@@ -5,14 +5,16 @@ from utils import MouseButton
 class EditTool(Tool):
     def __init__(self):
         self._component = None
+        self._offset = None
 
     def on_click(self, app, event, button, position, component):
-        if button == MouseButton.LEFT:
+        if button == MouseButton.LEFT and component is not None:
             self._component = component
+            self._offset = component.display.position - position
         elif button == MouseButton.RELEASE_LEFT:
             self._component = None
 
     def on_move(self, app, event, position):
         if self._component:
-            self._component.display.position = position
+            self._component.display.position = self._offset + position
             app.repaint()
