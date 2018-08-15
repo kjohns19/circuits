01b8746a5b3e9a7db908800aa2289eb5aea71849
Kevin Johnson <krjohnson111@gmail.com>
2018-08-15 18:35:51 -0400

    Can move component while creating it

--- tools/create_tool.py
+++ tools/create_tool.py
@@ -5,6 +5,7 @@ import utils
 class CreateTool(Tool):
     def __init__(self, creator=None):
         self._creator = creator
+        self._component = None
 
     @property
     def creator(self):
@@ -18,14 +19,27 @@ class CreateTool(Tool):
         if self._creator is None:
             return
 
+        if button == utils.MouseButton.RELEASE_LEFT:
+            self._component = None
         if button == utils.MouseButton.LEFT:
-            new_component = self._creator(app.circuit)
-            new_component.display.position = position
+            self._component = self._creator(app.circuit)
+            self._component.display.position = position
             app.repaint()
         elif button == utils.MouseButton.RIGHT and component:
+            if component is self._component:
+                component.delete()
+                app.repaint()
+                self._component = None
+                return
+
             def callback(selection):
                 if selection == 'Yes':
                     component.delete()
                     app.repaint()
 
             utils.show_popup('Delete?', ['Yes', 'No'], event, callback)
+
+    def on_move(self, app, event, position):
+        if self._component:
+            self._component.display.position = position
+            app.repaint()
