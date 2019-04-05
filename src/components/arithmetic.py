232f4d615e3bbd39cc82b32d5ed32af1d9f89bee
Kevin Johnson <krjohnson111@gmail.com>
2019-04-04 22:39:19 -0400

    Addition and multiplication components can support more inputs

--- src/components/arithmetic.py
+++ src/components/arithmetic.py
@@ -1,14 +1,20 @@
 import utils
 
+import functools
+import operator
+
 
 CATEGORY = 'Arithmetic'
 
 _operators = [
-    ('Add', lambda a, b: a+b),
-    ('Sub', lambda a, b: a-b),
-    ('Mul', lambda a, b: a*b),
-    ('Div', lambda a, b: a//b)
+    ('Add', (lambda *args: sum(args)), 2, 10),
+    ('Sub', (lambda a, b: a-b), 2, 2),
+    ('Mul', (lambda *args: functools.reduce(operator.mul, args)), 2, 10),
+    ('Div', (lambda a, b: a//b), 2, 2)
 ]
 
-for name, op in _operators:
-    utils.create_nary_component(name, CATEGORY, op, default_value=0)
+for name, op, min_inputs, max_inputs in _operators:
+    utils.create_nary_component(
+        name, CATEGORY, op,
+        min_inputs=min_inputs, max_inputs=max_inputs,
+        default_value=0)
