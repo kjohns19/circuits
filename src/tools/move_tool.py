b4618edb84e190c22cab47bd2cf608e5958265d6
Kevin Johnson <krjohnson111@gmail.com>
2019-04-09 20:07:17 -0400

    Rewrite move tool to be able to handle multiple components

--- src/tools/move_tool.py
+++ src/tools/move_tool.py
@@ -4,20 +4,26 @@ from .tool import Tool
 class MoveTool(Tool):
     def __init__(self):
         super().__init__()
-        self._move_component = None
-        self._offset = None
+        self._move_data = []
+
+    def _set(self, components, position):
+        self._move_data = [
+            (component, component.display.position - position)
+            for component in components
+        ]
 
     def on_left_click(self, app, event, position, component):
         if component is not None:
-            self._move_component = component
-            self._offset = component.display.position - position
+            self._set([component], position)
 
     def on_left_release(self, app, event, position, component):
-        self._move_component = None
+        self._move_data = []
 
     def on_move(self, app, event, position):
         super().on_move(app, event, position)
-        if self._move_component:
-            self._move_component.display.position = app.snap_position(
-                self._offset + position) - (0, app.grid_size/2)
+        if self._move_data:
+            for component, offset in self._move_data:
+                component.display.position = (
+                    app.snap_position(offset + position) - (0, app.grid_size/2)
+                )
             app.repaint()
